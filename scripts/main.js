eab5fdc375091fd74d075b87d0bf5f0ba21b853b
murathe <isaacsm07p@gmail.com>
2019-11-05 16:06:36 +0300

    Portfolio hover scripting

--- scripts/main.js
+++ scripts/main.js
@@ -26,4 +26,57 @@ $(document).ready(function () {
     $("#product").click(function (){
         $("#product p").toggle();
         $("#product img").toggle();
-    })
+    });
+
+    $(".0").mouseenter(function(){
+        $(".text-p").show(1000);
+    });
+    $(".0").mouseleave(function(){
+        $(".text-p").hide(500);
+    });
+    $(".1").mouseenter(function(){
+        $(".text-p1").show(1000);
+    });
+    $(".1").mouseleave(function(){
+        $(".text-p1").hide(500);
+    });
+    $(".2").mouseenter(function(){
+        $(".text-p2").show(1000);
+    });
+    $(".2").mouseleave(function(){
+        $(".text-p2").hide(500);
+    });
+    $(".3").mouseenter(function(){
+        $(".text-p3").show(1000);
+    });
+    $(".3").mouseleave(function(){
+        $(".text-p3").hide(500);
+    });
+    $(".4").mouseenter(function(){
+        $(".text-p4").show(1000);
+    });
+    $(".4").mouseleave(function(){
+        $(".text-p4").hide(500);
+    });
+    $(".5").mouseenter(function(){
+        $(".text-p5").show(1000);
+    });
+    $(".5").mouseleave(function(){
+        $(".text-p5").hide(500);
+    });
+    $(".6").mouseenter(function(){
+        $(".text-p6").show(1000);
+    });
+    $(".6").mouseleave(function(){
+        $(".text-p6").hide(500);
+    });
+    $(".7").mouseenter(function(){
+        $(".text-p7").show(1000);
+    });
+    $(".7").mouseleave(function(){
+        $(".text-p7").hide(500);
+    });
+
+
+    
+});
